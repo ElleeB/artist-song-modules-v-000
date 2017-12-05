0bb04833a0e5ccbd4a0bc794f6c58504bcc3ac27
Laura Trager <laura.trager@gmail.com>
2017-12-05 21:35:19 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -6,9 +6,11 @@ class Artist
 
   @@artists = []
 
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
+  extend Memorable
+
+#  def self.find_by_name(name)
+#    @@artists.detect{|a| a.name == name}
+#  end
 
   def initialize
     @@artists << self
@@ -19,13 +21,13 @@ class Artist
     @@artists
   end
 
-  def self.reset_all
-    self.all.clear
-  end
+#  def self.reset_all
+#    self.all.clear
+#  end
 
-  def self.count
-    @@artists.count
-  end
+#  def self.count
+#    @@artists.count
+#  end
 
   def add_song(song)
     @songs << song
